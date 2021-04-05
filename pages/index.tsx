3af4e0c1e49c2de5d7cf73bc3f7cf9e3626bda4d
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-04-05 18:07:53 -0300

    Mapa Adicionado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import { useEffect, useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 import styles from '../styles/Home.module.css'
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
-export default function Home({ data1, data2 }) {
+export default function Home({ educInfo, mapInfo }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +13,8 @@ export default function Home({ data1, data2 }) {
       </Head>
       <h1>United States Educational Attainment</h1>
       <h4>Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</h4>
-      <div style={{ height: '70vh', width: '70vw', margin: 'auto' }}>
-        <Graphic data1={data1} data2={data2} />
+      <div style={{ width: '70vw', margin: 'auto' }}>
+        <Graphic educInfo={educInfo} mapInfo={mapInfo} />
       </div>
     </div>
   )
@@ -24,42 +24,44 @@ function Graphic(props) {
 
   const graphicRef = useRef<HTMLDivElement>(null);
 
-  useEffect(function () {
+  useLayoutEffect(function () {
 
-    const svgHeight = graphicRef.current.clientHeight;
     const svgWidth = graphicRef.current.clientWidth;
+    const svgHeight = svgWidth * 0.58;
 
-    console.log(svgWidth,window.innerWidth,svgWidth/window.innerWidth)
+    const transformVal = 'scale(' + (props.mapInfo.transform.scale[0] * svgWidth / 10) + '),translate(' + -props.mapInfo.transform.translate[0] + ',' + -props.mapInfo.transform.translate[1] + ')';
+
+    console.log(svgWidth, window.innerWidth, svgWidth / window.innerWidth)
 
     const svg = d3.select(graphicRef.current)
       .append('svg')
-      .attr('height', '100%')
-      .attr('width', '100%')
+      .attr('height', svgHeight)
+      .attr('width', svgWidth)
 
     svg.append("path")
-      .datum(topojson.merge(props.data2, props.data2.objects.nation.geometries))
+      .datum(topojson.merge(props.mapInfo, props.mapInfo.objects.nation.geometries))
       .attr("fill", "#ddd")
       .attr("d", d3.geoPath())
-      .attr('transform','scale('+(props.data2.transform.scale[0] * svgWidth/9.5)+','+(props.data2.transform.scale[1] * svgHeight/3.5)+')')
+      .attr('transform', transformVal)
 
-    // svg.append("path")
-    //   .datum(topojson.mesh(props.data2, props.data2.objects.states, (a, b) => a !== b))
-    //   .attr("fill", "none")
-    //   .attr("stroke", "white")
-    //   .attr("stroke-linejoin", "round")
-    //   .attr("d", d3.geoPath())
-    //   .attr('transform','scale(0.5)')
+    svg.append("path")
+      .datum(topojson.mesh(props.mapInfo, props.mapInfo.objects.states, (a, b) => a !== b))
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("stroke-linejoin", "round")
+      .attr("d", d3.geoPath())
+      .attr('transform', transformVal)
 
-    // svg.append("path")
-    //   .datum(topojson.mesh(props.data2, props.data2.objects.counties, (a, b) => a !== b))
-    //   .attr("fill", "none")
-    //   .attr("stroke", "white")
-    //   .attr("stroke-linejoin", "round")
-    //   .attr("d", d3.geoPath())
-    //   .attr('transform','scale(0.5)')
+    svg.append("path")
+      .datum(topojson.mesh(props.mapInfo, props.mapInfo.objects.counties, (a, b) => a !== b))
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("stroke-linejoin", "round")
+      .attr("d", d3.geoPath())
+      .attr('transform', transformVal)
 
 
-    console.log(props.data2)
+    console.log(props.mapInfo)
 
     return (function () {
       svg.remove();
@@ -74,10 +76,10 @@ function Graphic(props) {
 export async function getStaticProps(context) {
   const res1 = await fetch(`https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json`)
   const res2 = await fetch(`https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json`)
-  const data1 = await res1.json()
-  const data2 = await res2.json()
+  const educInfo = await res1.json()
+  const mapInfo = await res2.json()
 
   return {
-    props: { data1, data2 }, // will be passed to the page component as props
+    props: { educInfo, mapInfo }, // will be passed to the page component as props
   }
 }
